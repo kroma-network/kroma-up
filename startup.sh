d89030cada711830d55bf68ddea35b9004cb3e98
Jinn <jinnuae40@gmail.com>
2023-09-05 14:40:59 +0900

    feat: add split network through parameter

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+NETWORK_NAME=$1
+
+# Check if proper argument is provided for network
+if [[ -z $NETWORK_NAME ]]; then
+    echo "Error: Network argument not provided. Usage: $0 <network>"
+    exit 1
+elif [[ $NETWORK_NAME != "sepolia" && $NETWORK_NAME != "mainnet" ]]; then
+    echo "Error: Invalid network. Allowed values are 'sepolia' or 'mainnet'."
+    exit 1
+fi
 
 # Create snapshot logs directory used in kroma-node
 mkdir -p keys logs
@@ -8,4 +18,4 @@ openssl rand -hex 32 > keys/jwt-secret.txt
 openssl rand -hex 32 > keys/p2p-node-key.txt
 
 # Create ENV specified by user.
-cp .env-sepolia.sample .env
+cp .env.$NETWORK_NAME .env
