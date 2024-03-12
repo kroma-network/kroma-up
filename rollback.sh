b866fd6e3d2b23d9051a379d2a69cae922c30de3
0xHansLee <hans@lightscale.io>
2024-03-12 19:34:16 +0900

    feat: rollback mainnet to v1.3.1

---
+++ rollback.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SNAPSHOT_ORIGIN=https://snapshot.kroma.network/latest/snapshot.tar.gz
+KROMA_DB_PATH=/.kroma/db/geth
+SNAPSHOT_PATH=.snapshot_rollback
+
+mkdir -p ${SNAPSHOT_PATH}
+wget -P ./${SNAPSHOT_PATH} ${SNAPSHOT_ORIGIN}
+tar xvzf ${SNAPSHOT_PATH}/snapshot.tar.gz -C ${SNAPSHOT_PATH}/
+rm -rf ${SNAPSHOT_PATH}/snapshot.tar.gz ${SNAPSHOT_PATH}/nodekey
+
+docker stop kroma-node
+
+docker exec -it kroma-geth sh -c "mv ${KROMA_DB_PATH}/nodekey ../nodekey"
+docker exec -it kroma-geth sh -c "rm -rf ${KROMA_DB_PATH}/*"
+docker exec -it kroma-geth sh -c "mv ../nodekey ${KROMA_DB_PATH}/nodekey"
+
+docker cp ${SNAPSHOT_PATH}/. kroma-geth:${KROMA_DB_PATH}/
+rm -rf ${SNAPSHOT_PATH}
+
+docker restart kroma-geth
+sleep 5
+docker start kroma-node
