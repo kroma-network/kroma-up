89e76f9b02df54a4c0649ec54010faced94ac572
Jinn <jinnuae40@gmail.com>
2024-03-19 18:14:10 +0900

    chore: improve snapshot speed

--- sync_block.sh
+++ sync_block.sh
@@ -6,30 +6,19 @@ if [[ -z $NETWORK_NAME ]]; then
   echo "Error: Argument not provided. Usage: $0 <network>. Allowed values are 'sepolia' or 'mainnet'."
   exit 1
 elif [[ $NETWORK_NAME == "sepolia" ]]; then
-  SNAPSHOT_ORIGIN=https://snapshot.sepolia.kroma.network/latest/snapshot.tar.gz
+  SNAPSHOT_ORIGIN=https://snapshot.sepolia.kroma.network/test/snapshot.tar.gz
 elif [[ $NETWORK_NAME == "mainnet" ]]; then
-  SNAPSHOT_ORIGIN=https://snapshot.kroma.network/latest/snapshot.tar.gz
+  SNAPSHOT_ORIGIN=https://snapshot.kroma.network/test/snapshot.tar.gz
 else
   echo "Error: Invalid network. Allowed values are 'sepolia' or 'mainnet'."
   exit 1
 fi
 
 KROMA_DB_PATH=/.kroma/db/geth
-SNAPSHOT_PATH=.snapshot_$(date +%s | base64 | cut -c 1-10)
-
-mkdir -p ${SNAPSHOT_PATH}
-wget -P ./${SNAPSHOT_PATH} ${SNAPSHOT_ORIGIN}
-tar xvzf ${SNAPSHOT_PATH}/snapshot.tar.gz -C ${SNAPSHOT_PATH}/
-rm -rf ${SNAPSHOT_PATH}/snapshot.tar.gz ${SNAPSHOT_PATH}/nodekey
 
 docker stop kroma-node
-
-docker exec -it kroma-geth sh -c "mv ${KROMA_DB_PATH}/nodekey ../nodekey"
-docker exec -it kroma-geth sh -c "rm -rf ${KROMA_DB_PATH}/*"
-docker exec -it kroma-geth sh -c "mv ../nodekey ${KROMA_DB_PATH}/nodekey"
-
-docker cp ${SNAPSHOT_PATH}/. kroma-geth:${KROMA_DB_PATH}/
-rm -rf ${SNAPSHOT_PATH}
+docker exec -it kroma-geth sh -c "rm -rf ${KROMA_DB_PATH}/chaindata"
+docker exec -it kroma-geth sh -c "cd /.kroma/db/geth && wget -O - ${SNAPSHOT_ORIGIN} | tar -xvz"
 
 docker restart kroma-geth
 sleep 5
